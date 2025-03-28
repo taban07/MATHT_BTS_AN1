3cdabb3360b67020b2d1d0ef9dfabb989231366c
taban07 <estebanolive07@hotmail.com>
2025-03-28 11:04:14 +0100

    envoi du code bataille

---
+++ olivero.esteban.bataille.py
@@ -0,0 +1,14 @@
+#fonction pour melanger le paquet
+def melanger (paquet) :
+    return paquet
+
+
+paquet = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+k = 0
+while k < 49 :
+    i = int(0, 13)
+    j = int(0. 13)
+    paquet[i] = temp
+    paquet[j] = paquet[i]
+    temp = paquet[j]
+    k + 1 = k
